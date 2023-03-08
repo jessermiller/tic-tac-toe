e2a5dc30b723ca43d62af0bfaab45f252e5efed0
jessermiller <iamjaymiller@yahoo.com>
2023-03-08 00:19:24 -0600

    added more html & js

---
+++ main.js
@@ -0,0 +1,18 @@
+const playerX = 'x';
+const playerO = 'o';
+
+const winningVariations = [
+    [0, 1, 2],
+    [3, 4, 5], 
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
+const board = document.getElementById('board');
+const tableCells = document.querySelectorAll('[data-cell]');
+const restartBtn = document.getElementById('restartBtn');
+const winningMessage = document.getElementById('winningMessage');
